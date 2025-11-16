9a75d1388f85f446c89532991b63ba099ba1b1a5
Arjun Rawat <arjunrawat4741@gmail.com>
2025-11-16 19:30:57 +0200

    fix: Remove unused imports and props from ScreenshotCard component

--- frontend/src/components/ScreenshotCard.tsx
+++ frontend/src/components/ScreenshotCard.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
-import ScreenshotCarousel from './ScreenshotCarousel';
 import PhoneMockup from './PhoneMockup';
 
-interface Props {
-  title?: string;
-  subtitle?: string;
-  imageSrc?: string | undefined;
-}
-
-const ScreenshotCard: React.FC<Props> = ({ title = 'Callers', subtitle = 'WEBSITE', imageSrc }) => {
+const ScreenshotCard: React.FC = () => {
   return (
     <div className="relative w-full flex items-center justify-center">
       {/* Phone mockup - no background box */}
       <div className="relative z-10 scale-95 md:scale-100">
-        <PhoneMockup imageUrl="https://images.pexels.com/photos/1802268/pexels-photo-1802268.jpeg" />
+        <PhoneMockup />
       </div>
     </div>
   );
